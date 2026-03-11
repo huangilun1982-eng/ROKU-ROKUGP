988ae0c87b9e6305fb10b994308b6b4129caf13f
unknown <huangilun1982@gmail.com>
2026-03-11 11:01:37 +0800

    fix: 修正 _auto_load_base_life 中錯誤的 combo 名稱

--- ROKU_G66_Editor/ui_main_window.py
+++ ROKU_G66_Editor/ui_main_window.py
@@ -596,7 +596,7 @@ class MainWindow(QMainWindow):
             return
             
         tool_mat = 'CARBIDE' if self.combo_tool_mat.currentText() == '鎢鋼 (Carbide)' else 'HSS'
-        work_mat = self.combo_material.currentData() or 'SUS420'
+        work_mat = self.combo_work_mat.currentData() or 'SUS420'
         base_meters = self.config.data.get('base_life_meters', {}).get(tool_mat, {}).get(work_mat, 20.0)
         
         self.spin_base_life_meters.blockSignals(True)
